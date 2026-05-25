fa2f59949d9c9055c990da28237feb3d60c56a87
dlwpals7 <xodzm2367@gmail.com>
2026-05-25 15:27:51 +0900

    Character&BattleManager

--- Main.java
+++ Main.java
@@ -1,43 +1,117 @@
-import java.util.Scanner;
-import java.math.BigInteger;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+abstract class Character {
+    String name, 
+    postposition_1,    //을/를
+    postposition_2,    //이/가
+    postposition_3;    //은/는
+    int maxHp, currentHp, damage, armour;
+    
+    Character(String name, int maxHp, int damage, int armour) {
+        this.name = name;
+        this.maxHp = maxHp;
+        this.currentHp = maxHp;
+        this.damage = damage;
+        this.armour = armour;
+
+        boolean postposition = true;        //모음으로 끝나는 경우
+        if (this.name.charAt(this.name.length()-1) >= '가' && this.name.charAt(this.name.length()-1) <= '힣') {  //자음으로 끝나는 경우
+            if ((this.name.charAt(this.name.length()-1) - '가') % 28 != 0) {
+                postposition = false;
+            }
+        }
+        this.postposition_1 = postposition ? "를 " : "을 ";
+        this.postposition_2 = postposition ? "가 " : "이 ";
+        this.postposition_3 = postposition ? "는 " : "은 ";
+    }
+
+    void melee(int damage) {
+        this.currentHp -= damage;
+    }
+}
+
+class Player extends Character {
+    Player(String name) {
+        super(name, 100, 10, 10);
+    }
+}
+
+class Monster extends Character {
+    Monster(String name, int maxHp, int damage, int armour) {
+        super(name, maxHp, damage, armour);
+    }
+}
+
+
+class BattleManager {
+    //BattleManager() {}
+    private int intInput = 0;
+    
+    void actionSelection(Character actor) {
+        System.out.println("-------------------");
+        System.out.println("뭘할까?\n1. 공격\n2. 방어\n3. 회피\n0. 종료\n");
+    }
+    
+    void battleInfrom(Character actor1, Character actor2) {
+        
+        int realDamage = (actor1.damage * 100) / (100 + (int)Math.pow(actor2.armour, 2));
+        System.out.println("-------------------");
+        System.out.print(actor1.name + actor1.postposition_2);
+        System.out.println(actor2.name + actor2.postposition_1 + "공격!");
+        System.out.print(actor1.name + actor1.postposition_2);
+        System.out.println("입힌 피해량: " + realDamage);
+        System.out.print(actor2.name + "의 체력 변동치: " + actor2.currentHp + " -> ");
+        actor2.melee(realDamage);
+        System.out.println(actor2.currentHp + "\n");
+    }
+
+    void dodgeInfrom(Character actor1, Character actor2) {
+        
+        if ((int)(Math.random() * 100) >= 50) {
+            System.out.println("-------------------");
+            System.out.print(actor1.name + actor1.postposition_2);
+            System.out.println(actor2.name + actor2.postposition_1 + "공격!");
+            System.out.println(actor2.name + actor2.postposition_3 + "피하려 했지만 실패했다!");
+            System.out.print(actor1.name + actor1.postposition_2);
+            System.out.println("입힌 피해량: " + actor1.damage);
+            System.out.print(actor2.name + "의 체력 변동치: " + actor2.currentHp + " -> ");
+            actor2.melee(actor1.damage);
+            System.out.println(actor2.currentHp + "\n");
+        } else {
+            System.out.println("-------------------");
+            System.out.print(actor1.name + actor1.postposition_2);
+            System.out.println(actor2.name + actor2.postposition_1 + "공격!");
+            System.out.println("하지만 " + actor2.name + actor2.postposition_2 + "간단히 피해버렸다!\n");
+        }
+    }
+
+    void actionJunction(int intInput, Character actor1, Character actor2) {
+        switch (intInput) {
+            case 1 -> battleInfrom(actor1, actor2);
+            case 2 -> battleInfrom(actor2, actor1);
+            case 3 -> dodgeInfrom(actor2, actor1);
+            default -> System.out.println("전투 개시!");
+        }
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        // 1st Step
-        Scanner scanner = new Scanner(System.in);
-        // [pq] > 1
-        BigInteger p = new BigInteger(scanner.next());
-        BigInteger q = new BigInteger(scanner.next());
-
-        // [pq] == prime_number
-        if (!p.isProbablePrime(10) || !q.isProbablePrime(10)) {
-            System.out.println("Composite Number Detected!");
-            return;
-        }
+        Scanner input = new Scanner(System.in);
+        int intInput = 0;
         
-        // N = p * q
-        BigInteger N = p.multiply(q);
-        // phi(N) = (p - 1) * (q - 1)
-        BigInteger phiN = (p.subtract(BigInteger.valueOf(1))).multiply(q.subtract(BigInteger.valueOf(1))); 
-        BigInteger e = new BigInteger("65537");
-        BigInteger d = e.modInverse(phiN);
+        Character adventurer = new Player("모험가");
+        Character goblin = new Monster("고블린", 50, 10, 5);
+        BattleManager BattleManager = new BattleManager();
         
-        System.out.println("p: " + p + "\nq: " + q + "\nN: " + N + "\nphiN: " + phiN + "\ne: " + e + "\nd: " + d);
+        do {
+            BattleManager.actionJunction(intInput, adventurer, goblin);
+            BattleManager.actionSelection(adventurer);
+            intInput = input.nextInt();
+        } while (intInput != 0);
 
-        // 2nd Step
-        BigInteger M = new BigInteger(scanner.next());
-        if(M.compareTo(N) >= 1) {
-            System.out.println("Message Out Of Bounds!");
-            return;
-        }
         
-        // C = M^e (mod N) 
-        BigInteger C = M.modPow(e, N);
-        System.out.println("M: " + M + "\nC: " + C);
-
-        // 3rd Step
-        // M = C^d (mod N)
-        BigInteger R = C.modPow(d, N);
-        System.out.println("R: " + R);
     }
 }
