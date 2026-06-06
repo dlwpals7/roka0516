fa316a742a1af132cadde01ed5684da6c3618041
dlwpals7 <xodzm2367@gmail.com>
2026-06-06 17:15:08 +0900

    1st Update Main.java

--- Main.java
+++ Main.java
@@ -4,15 +4,21 @@ import java.io.*;
 
 abstract class Character {
     String name, 
-    postposition_1,    //을/를
-    postposition_2,    //이/가
-    postposition_3;    //은/는
-    int maxHp, currentHp, damage, armour;
+    postposition_1,           // 을/를
+    postposition_2,           // 이/가
+    postposition_3;           // 은/는
     
-    Character(String name, int maxHp, int damage, int armour) {
+    int maxHp, currentHp,     // 체력
+    maxEnergy, currentEnergy,  // 활력
+    damage,                   // 공격
+    armour;                   // 방어
+    
+    Character(String name, int maxHp, int maxEnergy, int damage, int armour) {
         this.name = name;
         this.maxHp = maxHp;
         this.currentHp = maxHp;
+        this.maxEnergy = maxEnergy;
+        this.currentEnergy = maxEnergy;
         this.damage = damage;
         this.armour = armour;
 
@@ -27,20 +33,67 @@ abstract class Character {
         this.postposition_3 = postposition ? "는 " : "은 ";
     }
 
-    void melee(int damage) {
+    void modifyHpNeg(int damage) {
         this.currentHp -= damage;
+        this.currentHp = this.currentHp > this.maxHp ? this.maxHp : (this.currentHp < 0 ? 0 : this.currentHp);
+    }
+
+    boolean isAlive() {
+        return this.currentHp != 0;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    String getPostposition(int index) {
+        return switch (index) {
+            case 1 -> this.postposition_1;
+            case 2 -> this.postposition_2;
+            case 3 -> this.postposition_3;
+            default -> "Invalid Index";
+        };
     }
 }
 
 class Player extends Character {
+    int maxLevel = 6, currentLevel = 0, exp = 0;
+    // x-1 + x*x
+    int[] expTable = { 1, 5, 11, 19, 29, 41 }
+    
     Player(String name) {
-        super(name, 100, 10, 10);
+        super(name, 100, 100, 20, 10);
+    }
+
+    void gainExp(int value) {
+        if (this.currentLevel >= this.maxLevel) return;
+        this.exp += value;
+        while (this.exp < this.expTable[this.currentLevel]) {
+            this.exp -= this.expTable[this.currentLevel];
+            this.currentLevel += 1;
+        }
     }
 }
 
 class Monster extends Character {
-    Monster(String name, int maxHp, int damage, int armour) {
-        super(name, maxHp, damage, armour);
+    int rewardExp,         // 보상 경험치
+    rewardGold;            // 보상 금화
+    
+    Monster(String name, int maxHp, int maxEnergy, int damage, int armour, int rewardExp, int rewardGold) {
+        super(name, maxHp, maxEnergy, damage, armour);
+        this.rewardExp = rewardExp;
+        this.rewardGold = rewardGold;
+    }
+}
+
+class MonsterPool {
+    private MonsterPool() {}
+    // name, maxHp, maxEnergy, damage, armour, rewardExp, rewardGold
+    public static Monster getGoblin() {
+        return new Monster("고블린", 50, 50, 10, 5, 1, 1);
+    }
+    public static Monster getOrc() {
+        return new Monster("오크", 150, 75, 60, 30, 6, 3);
     }
 }
 
@@ -63,7 +116,7 @@ class BattleManager {
         System.out.print(actor1.name + actor1.postposition_2);
         System.out.println("입힌 피해량: " + realDamage);
         System.out.print(actor2.name + "의 체력 변동치: " + actor2.currentHp + " -> ");
-        actor2.melee(realDamage);
+        actor2.modifyHpNeg(realDamage);
         System.out.println(actor2.currentHp + "\n");
     }
 
@@ -77,7 +130,7 @@ class BattleManager {
             System.out.print(actor1.name + actor1.postposition_2);
             System.out.println("입힌 피해량: " + actor1.damage);
             System.out.print(actor2.name + "의 체력 변동치: " + actor2.currentHp + " -> ");
-            actor2.melee(actor1.damage);
+            actor2.modifyHpNeg(actor1.damage);
             System.out.println(actor2.currentHp + "\n");
         } else {
             System.out.println("-------------------");
@@ -103,15 +156,27 @@ public class Main {
         int intInput = 0;
         
         Character adventurer = new Player("모험가");
-        Character goblin = new Monster("고블린", 50, 10, 5);
-        BattleManager BattleManager = new BattleManager();
+        Character goblin_1 = MonsterPool.getGoblin();
+        Character orc_1 = MonsterPool.getOrc();
+        BattleManager battleManager = new BattleManager();
         
-        do {
-            BattleManager.actionJunction(intInput, adventurer, goblin);
-            BattleManager.actionSelection(adventurer);
+        while (true) {
+            battleManager.actionSelection(adventurer);
             intInput = input.nextInt();
-        } while (intInput != 0);
+            if (intInput == 0) { break; }
+            battleManager.actionJunction(intInput, adventurer, goblin_1);
+            
+            if (!goblin_1.isAlive()) {
+                System.out.println(goblin_1.getName() + goblin_1.getPostposition(3) + " 쓰러졌다!\n");
+                break;
+            }
+        } 
 
-        
+        while (intInput != 0) {
+            battleManager.actionJunction(intInput, adventurer, orc_1);
+            battleManager.actionSelection(adventurer);
+            intInput = input.nextInt();
+        }
+        System.out.println("-------------------\n종료");
     }
 }
